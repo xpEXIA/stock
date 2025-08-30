c0e702dc67bbc905d066c1c1814e6c971b84fd8a
xpexia <773532884@qq.com>
2025-08-30 18:55:40 +0800

    在dm_stock_performance中添加vol_20_pct和first_double_vol计算逻辑

--- stockDataETL/dataTransform/commonUtils/get_pretrade_date.py
+++ stockDataETL/dataTransform/commonUtils/get_pretrade_date.py
@@ -1,19 +1,41 @@
 from datetime import datetime
+
+from pandas import DataFrame
+
 from stockDataETL.dataLoad.DataLoad import DataLoad
 
 
-def get_pretrade_date(trade_date: str) -> str:
+def get_pretrade_date(
+        trade_date: str,
+        interval: int = 0
+) -> str:
+
+    """
+    获取指定交易日的前一个交易日
+    :param trade_date: 交易日，仅支持"%Y-%m-%d"
+    :return: 前一个交易日
+    """
 
     data_load = DataLoad()
     pretrade_date = data_load.search(
         """
-        select pretrade_date
+        select 
+            cal_date,
+            pretrade_date
         from ods_trade_cal
-        where cal_date = :trade_date
+        where cal_date >= date_sub(:trade_date, interval :interval day) and cal_date <= :trade_date
         """,
         {
-            "trade_date": datetime.strptime(trade_date, "%Y-%m-%d").strftime("%Y%m%d")
+            "trade_date": datetime.strptime(trade_date, "%Y-%m-%d").strftime("%Y%m%d"),
+            "interval": interval
             # "trade_date": "20250716"
         }
-    )[0][0]
-    return datetime.strptime(pretrade_date, "%Y%m%d").strftime("%Y-%m-%d")
+    )
+    pretrade_date = DataFrame(pretrade_date, columns=[
+        "cal_date", "pretrade_date"
+    ])
+
+    if interval == 0:
+        return datetime.strptime(pretrade_date['pretrade_date'].values[0], "%Y%m%d").strftime("%Y-%m-%d")
+    else:
+        return pretrade_date
--- stockDataETL/dataTransform/dm_stock_performance_daily.py
+++ stockDataETL/dataTransform/dm_stock_performance_daily.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
 from os import rename
+from tkinter.constants import ROUND
+
 import pandas as pd
 from pandas import DataFrame
 from stockDataETL import logger
 from stockDataETL.dataLoad.DataLoad import DataLoad
+from stockDataETL.dataTransform.commonUtils.get_pretrade_date import get_pretrade_date
 from stockDataETL.dataTransform.commonUtils.trade_date_complete_check import trade_date_complete_check
 
 
@@ -22,6 +25,7 @@ def dm_stock_performance_daily(
             name,
             industry,
             close,
+            vol,
             open_pct_chg,
             high_pct_chg,
             pct_chg,
@@ -36,9 +40,10 @@ def dm_stock_performance_daily(
         }
     )
     data = DataFrame(data, columns=[
-        "trade_date", "ts_code", "name", "industry","close",
+        "trade_date", "ts_code", "name", "industry","close","vol",
         "open_pct_chg", "high_pct_chg","pct_chg","up_limit","down_limit",
     ])
+    data['trade_date'] = data['trade_date'].apply(lambda x: x.strftime("%Y-%m-%d"))
 
     up_days = data[data['pct_chg'] > 0].groupby(["ts_code", "name", "industry"]).agg(
         up_days=("pct_chg", "count")
@@ -59,21 +64,29 @@ def dm_stock_performance_daily(
         down_limit_days=("pct_chg", "count")
     ).reset_index().rename(columns={"pct_chg": "down_limit_days"})
 
+    pretrade_date = get_pretrade_date(trade_date, interval)
+    pretrade_date["cal_date"] = (pretrade_date["cal_date"]
+                     .apply(lambda x: datetime.strptime(x, "%Y%m%d").strftime("%Y-%m-%d")))
+    pretrade_date["pretrade_date"] = (pretrade_date["pretrade_date"]
+                     .apply(lambda x: datetime.strptime(x, "%Y%m%d").strftime("%Y-%m-%d")))
     def _cal_pct_chg(
             data,
             up_data,
             cal_col,
             cal_fun,
             groupby,
-            merge_on
+            merge_on,
+            pretrade_date
         ):
 
+        pretrade_date=pretrade_date
         up_data["trade_date"] = up_data["trade_date"].apply(
             # lambda x: (
             #     datetime.strptime(x, "%Y-%m-%d") + timedelta(days=1)
             # ).strftime("%Y-%m-%d")
-            lambda x: (x + timedelta(days=1))
+            lambda x: (pretrade_date["pretrade_date"][pretrade_date["cal_date"] == x].values[0])
         )
+
         return pd.merge(
             up_data,
             data,
@@ -90,7 +103,8 @@ def dm_stock_performance_daily(
         "open_pct_chg",
         "mean",
         "ts_code",
-        ["trade_date", "ts_code"]
+        ["trade_date", "ts_code"],
+        pretrade_date
     ).rename(columns={"cal_col": "up_5_next_open_pct_chg"})
 
     up_5_next_high_pct_chg = _cal_pct_chg(
@@ -99,7 +113,8 @@ def dm_stock_performance_daily(
         "high_pct_chg",
         "mean",
         "ts_code",
-        ["trade_date", "ts_code"]
+        ["trade_date", "ts_code"],
+        pretrade_date
     ).rename(columns={"cal_col": "up_5_next_high_pct_chg"})
 
     up_5_next_close_pct_chg = _cal_pct_chg(
@@ -108,7 +123,8 @@ def dm_stock_performance_daily(
         "pct_chg",
         "mean",
         "ts_code",
-        ["trade_date", "ts_code"]
+        ["trade_date", "ts_code"],
+        pretrade_date
     ).rename(columns={"cal_col": "up_5_next_close_pct_chg"})
 
     up_limit_data = data[data['close'] == data['up_limit']][["trade_date", "ts_code"]]
@@ -118,7 +134,8 @@ def dm_stock_performance_daily(
         "open_pct_chg",
         "mean",
         "ts_code",
-        ["trade_date", "ts_code"]
+        ["trade_date", "ts_code"],
+        pretrade_date
     ).rename(columns={"cal_col": "up_limit_next_open_pct_chg"})
 
     up_limit_next_high_pct_chg = _cal_pct_chg(
@@ -127,7 +144,8 @@ def dm_stock_performance_daily(
         "high_pct_chg",
         "mean",
         "ts_code",
-        ["trade_date", "ts_code"]
+        ["trade_date", "ts_code"],
+        pretrade_date
     ).rename(columns={"cal_col": "up_limit_next_high_pct_chg"})
 
     up_limit_next_close_pct_chg = _cal_pct_chg(
@@ -136,11 +154,45 @@ def dm_stock_performance_daily(
         "pct_chg",
         "mean",
         "ts_code",
-        ["trade_date", "ts_code"]
+        ["trade_date", "ts_code"],
+        pretrade_date
     ).rename(columns={"cal_col": "up_limit_next_close_pct_chg"})
 
     trade_days = (data.groupby('ts_code')['pct_chg'].count().
                   reset_index().rename(columns={"pct_chg": "trade_days"}))
+
+    trade_date_duplicate = (data[['trade_date','vol']]
+                            .drop_duplicates(subset=['trade_date'],keep='first')
+                            .sort_values(by='trade_date',ascending=False))
+    vol_start_date = trade_date_duplicate['trade_date'].iloc[20]
+    vol_end_date = trade_date_duplicate['trade_date'].iloc[1]
+    mean_vol = (data[['trade_date',"ts_code", "vol"]][(data['trade_date'] >= vol_start_date)
+                                                     & (data['trade_date'] <= vol_end_date)]
+                .groupby('ts_code')
+                .agg(mean_vol=('vol', 'mean'))
+                .reset_index())
+    vol_20_pct = (data[['ts_code','vol']][data['trade_date'] == trade_date]
+                  .merge(
+        mean_vol,
+        how="left",
+        on=['ts_code']
+    ))
+    vol_20_pct['vol_20_pct'] = vol_20_pct['vol'] / vol_20_pct['mean_vol']
+
+    pretrade_date = get_pretrade_date(trade_date)
+    first_double_vol = (data[['ts_code','vol']][data['trade_date'] == trade_date]
+                        .merge(
+        data[['ts_code','vol']][data['trade_date'] == pretrade_date],
+        how="left",
+        on=['ts_code']
+    ))
+    # if first_double_vol['vol_x'] / first_double_vol['vol_y'] >2.5:
+    #     first_double_vol['first_double_vol'] = 1
+    # else:
+    #     first_double_vol['first_double_vol'] = 0
+    first_double_vol['first_double_vol'] = (
+            first_double_vol['vol_x'] / first_double_vol['vol_y'] > 2.5
+    ).astype(int)
 
     dm_stock_performance_data = up_days.merge(
         down_days,
@@ -190,7 +242,16 @@ def dm_stock_performance_daily(
         trade_days,
         how="left",
         on=["ts_code"]
+    ).merge(
+        vol_20_pct[["ts_code", "vol_20_pct"]],
+        how="left",
+        on=["ts_code"]
+    ).merge(
+        first_double_vol[['ts_code', 'first_double_vol']],
+        how="left",
+        on=["ts_code"]
     )
+
     dm_stock_performance_data["trade_date"] = trade_date
     dm_stock_performance_data.fillna(0, inplace=True)
     dm_stock_performance_data[
@@ -201,6 +262,7 @@ def dm_stock_performance_daily(
             "up_limit_next_open_pct_chg",
             "up_limit_next_high_pct_chg",
             "up_limit_next_close_pct_chg",
+            'vol_20_pct'
         ]
     ] = dm_stock_performance_data[
         [
@@ -210,6 +272,7 @@ def dm_stock_performance_daily(
             "up_limit_next_open_pct_chg",
             "up_limit_next_high_pct_chg",
             "up_limit_next_close_pct_chg",
+            'vol_20_pct'
         ]
     ].apply(lambda x: round(x, 4))
 
