1181929a09235536c885d32c742b19ac61f26db0
xpexia <773532884@qq.com>
2026-05-12 09:02:17 +0800

    test

--- stockDataETL/manualAnalysis/sql_analysis.sql
+++ stockDataETL/manualAnalysis/sql_analysis.sql
@@ -1,1 +1,11 @@
-select * from stock.dm_up_limit_statistics where trade_date >= '2'
+select
+    *
+from stock.dw_daily_trends
+where trade_date = '2026-4-24' and market != '北交所'
+order by net_mf_amount,net_d5_amount,ctu_up_days desc
+
+select * from dm_daily_vol_unusual where trade_date = '2026-4-27'
+
+
+
+
